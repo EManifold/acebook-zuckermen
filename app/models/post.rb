2ff76b5e750edede008eb3ec2d51a48def9f2441
dbacall <dbacall@hotmail.co.uk>
2020-01-24 14:09:48 +0000

    style: small fixes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
   def line_break
     self.message = message.gsub(/\n/, "<br>").html_safe
--- spec/features/posts/posts_can_have_line_breaks_spec.rb
+++ spec/features/posts/posts_can_have_line_breaks_spec.rb
@@ -4,6 +4,6 @@ RSpec.feature "line breaks in posts" do
   scenario "posts can have line breaks in them" do
     sign_up('Harry', 'Mumford', 'hmumford', 'harry_mumford@hotmail.co.uk', 'password')
     new_post("This post\nhas line breaks")
-    page.html.should include('<p>This post<br>has line breaks</p>')
+    page.html.should include('<p class="post-message">This post<br>has line breaks</p>')
   end
 end
